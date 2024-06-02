3da1cd218bdc842671152138df67d4ccf4f3fa2a
Caikun Peng <148462372+Caikun-Peng@users.noreply.github.com>
2024-06-02 16:48:58 +1000

    06/02/2024 16:48:58 \n lab_1_functions.py \n\t Add: system_clk \n\t Update: \n\t\t Packet: __str__ discribes \n\t\t Source: return whole packets \n\t\t Server: finish state machine in service()

--- lab_1_functions.py
+++ lab_1_functions.py
@@ -10,7 +10,15 @@ Dependencies: numpy
 
 import numpy as np
 
-global clk 
+sys_clk = 0 
+
+class system_clk:
+	def __init__(self, clk=0):
+		global sys_clk 
+		sys_clk = clk
+	
+	def __str__(self):
+		return "Current system time: {}".format(f"{sys_clk:.3f}")
 
 class Packet:
 	def __init__(self, packet_index, time_arrivel, packet_size):
@@ -20,12 +28,15 @@ class Packet:
 		self.size      = packet_size
 
 	def __str__(self):
-		return "Index: {}, Arrival time: {}, Packet size: {}".format(self.index, self.arrival, self.size)
+		global sys_clk
+		return "Current time: {}\t Index: {}\t Packet size: {}\t Arrival time: {}\t Departure time: {}"\
+				.format(f"{sys_clk:.3f}", self.index, self.size, self.arrival, self.departure)
 
 class Source:
 	def __init__(self, _lambda = 1, npkts = 1000000, size = 1250):
 		self._lambda = _lambda
 		self.packet_count = npkts
+		self.packets = []
 		self.size = size
 		self.generated_packets = 0
 		self.current_time = 0
@@ -41,7 +52,9 @@ class Source:
 			packet = Packet(self.generated_packets, arrival_time, packet_size)
 			self.generated_packets += 1
 			self.current_time = arrival_time
-			yield packet
+			self.packets.append(packet)
+			
+		return self.packets
 
 class Queue:
 	def __init__(self, size = 1):
@@ -70,42 +83,72 @@ class Server:
 	State Transfer
 	# INITIAL -> GET_PACKET -> SERVE -> GET_PACKET
 	"""
-	def __init__(self):
-		self.service_rate = 10 * (10**9)  # The server can process 10Gbps
+	def __init__(self, service_rate = 1250):
+		# The server can process 10Gbps -> 10*10^9 bps -> 10*10^3 bits per us -> 1250 Bytes per us
+		self.service_rate = service_rate
 		self.current_time = 0
+		self.packet = None
 		self.state_current = "INITIAL"
 		self.state_next = "INITIAL"
 		self.state_falg = None
+		self.time_flag = 1
+		self.time_temp = 0
 
-	def state_update(self): 
-		self.state_current = self.state_next
+	def service(self, queue):
 
-	def state_transfer(self):
+		def state_update(self): 
+			self.state_current = self.state_next
+
+		def state_transfer(self):
+			match self.state_current:
+				case "INITIAL":
+					self.state_next = "GET_PACKET"
+				case "GET_PACKET":
+					if self.state_falg == "packet_getted":
+						self.state_next = "SERVE"
+					else: 
+						self.state_next = "GET_PACKET"
+				case "SERVE":
+					if self.state_falg == "packet_served":
+						self.state_next = "INITIAL"
+					else:
+						self.state_next = "SERVE"
+				case _:
+					self.state_next = "INITIAL"
+
+		def state_initial(self):
+			self.packet = None
+			self.state_falg = None
+			self.time_flag = 1
+			self.time_temp = 0
+
+		def state_get_packet(self, queue):
+			packet_temp = queue.extract()
+			if packet_temp != None:
+				self.packet = packet_temp
+				self.state_falg = "packet_getted"
+
+		def state_serve(self):
+			self.current_time = sys_clk
+			if self.time_flag:
+				self.time_temp = self.current_time
+				self.service_time = round(self.packet.size / self.service_rate, 3)
+				self.time_flag = 0
+		
+			time_delta = self.current_time - self.time_temp
+			if  time_delta >= self.service_time:
+				self.packet.departure = f"{self.current_time:.3f}"
+				print(self.packet)
+				self.state_falg = "packet_served"
+		
 		match self.state_current:
 			case "INITIAL":
-				self.state_next = "GET_PACKET"
+				state_initial(self)
 			case "GET_PACKET":
-				if self.state_falg == "packet_getted":
-					self.state_next = "SERVE"
-				else: 
-					self.state_next = "GET_PACKET"
+				state_get_packet(self, queue)
 			case "SERVE":
-				if self.state_falg == "packet_served":
-					self.state_next = "GET_PACKET"
-				else:
-					self.state_next = "SERVE"
+				state_serve(self)
 			case _:
-				self.state_next = "INITIAL"
-
-	def state_initial(self):
-		__init__()
-
-	def state_get_packet(self, packet):
-		packet_temp = packet
-		if packet_temp != None:
-			self.packet = packet_temp
-			self.state_falg = "packet_getted"
-
-	def state_serve(self):
-		service_time = self.packet.size / self.service_rate
-
+				state_initial(self)
+		state_transfer(self)
+		state_update(self)
