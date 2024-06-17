0b90f83ce69dd7260c062974286da4249d73be78
Caikun Peng <148462372+Caikun-Peng@users.noreply.github.com>
2024-06-17 18:52:08 +1000

    Update dependencies

--- Lab_2/fat_tree.py
+++ Lab_2/fat_tree.py
@@ -4,7 +4,7 @@ File Name: fat_tree.py
 Create Date: 16/06/2024 
 Last Edit Date: 17/06/2024
 Description: Generate fat-tree topo with parameter k
-Dependencies: mininet
+Dependencies: argparse, mininet
 '''
 
 import argparse
