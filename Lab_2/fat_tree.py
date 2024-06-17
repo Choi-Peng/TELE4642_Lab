b592aae671038cb5b3991eb34012c0072eb8cb14
Caikun Peng <148462372+Caikun-Peng@users.noreply.github.com>
2024-06-17 13:34:43 +1000

    Create fat_tree.py

---
+++ Lab_2/fat_tree.py
@@ -0,0 +1,11 @@
+'''
+Code Author: Peng, Caikun
+File Name: fat_tree.py
+Create Date: 16/06/2024 
+Last Edit Date: 17/06/2024
+Description: Generate fat-tree topo with parameter k
+Dependencies: mininet
+'''
+
+
+
