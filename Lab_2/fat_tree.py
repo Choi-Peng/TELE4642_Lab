260eb58bb4ea6244ba999f6aaf93ac194e59dab4
Caikun Peng <148462372+Caikun-Peng@users.noreply.github.com>
2024-06-18 10:44:04 +1000

    Update: mask replace prefix_len in routing_table

--- Lab_2/fat_tree.py
+++ Lab_2/fat_tree.py
@@ -40,12 +40,13 @@ def main(k):
             # create edge switch
             switch_num  = i
             switch_id   = i
-            switch_dpid = f'0000000000{pod}{switch_num}01'
+            switch_dpid = f'0000000000{pod:02x}{switch_num:02x}01'
             switch_name = f'edSw{pod}{switch_id}'
+            info(f'Creating edge switch {switch_name}')
             switch_edge = net.addSwitch(switch_name, 
                                         dpid = switch_dpid)
             pod_switches_edge.append(switch_edge)
-            routing_table[switch_name] = { # initial routing table
+            routing_table[switch_dpid] = { # initial routing table
                 "routes": [], 
                 "suffix_routes": []
             }
@@ -53,24 +54,25 @@ def main(k):
             for host_id in range (k // 2):
                 host_name = f'h{pod}{switch_id}{host_id}'
                 host_ip   = f'10.{pod}.{switch_id}.{host_id+2}'
+                info(f'Creating host {host_name}')
                 host      = net.addHost(host_name, ip = host_ip)
                 # create links between edge switch and host
                 net.addLink(host, switch_edge)
                 # add ronte rules for edge switches
                 route = {
                     "prefix"     : host_ip,
-                    "prefix_len" : 32,
+                    "mask"       : 0xffffffff,
                     "priority"   : priority_down,
                     "output"     : host_id + 1
                 }
                 suffix_route = {
                     "suffix"     : f'0.0.0.{host_id+2}',
-                    "prefix_len" : 32, 
+                    "mask"       : 0x000000ff, 
                     "priority"   : priority_up, 
                     "output"     : host_id + 1 + (k // 2)
                 }
-                routing_table[switch_name]["routes"].append(route)
-                routing_table[switch_name]["suffix_routes"].append(suffix_route)
+                routing_table[switch_dpid]["routes"].append(route)
+                routing_table[switch_dpid]["suffix_routes"].append(suffix_route)
 
             
         # create aggr switches with links
@@ -78,8 +80,9 @@ def main(k):
             # create aggr switch
             switch_num  = (k // 2) + i
             switch_id   = i
-            switch_dpid = f'0000000000{pod}{switch_num}01'
+            switch_dpid = f'0000000000{pod:02x}{switch_num:02x}01'
             switch_name = f'agSw{pod}{switch_id}'
+            info(f'Creating aggr switch {switch_name}')
             switch_aggr = net.addSwitch(switch_name, 
                                         dpid = switch_dpid)
             pod_switches_aggr.append(switch_aggr)
@@ -87,25 +90,25 @@ def main(k):
             for switch_edge in pod_switches_edge:
                 net.addLink(switch_aggr, switch_edge)
             # add ronte rules for aggr switches
-            routing_table[switch_name] = { # initial routing table
+            routing_table[switch_dpid] = { # initial routing table
                 "routes": [], 
                 "suffix_routes": []
             }
             for host_id in range (k // 2):
                 route = {
-                    "suffix"     : f'10.0.{host_id}.0',
-                    "prefix_len" : 24,
+                    "prefix"     : f'10.0.{host_id}.0',
+                    "mask"       : 0xffffff00,
                     "priority"   : priority_down,
                     "output"     : host_id + 1
                 }
                 suffix_route = {
                     "suffix"     : f'0.0.{host_id}.0',
-                    "prefix_len" : 24, 
+                    "mask"       : 0x0000ff00, 
                     "priority"   : priority_up, 
                     "output"     : host_id + 1 + (k // 2)
                 }
-                routing_table[switch_name]["routes"].append(route)
-                routing_table[switch_name]["suffix_routes"].append(suffix_route)
+                routing_table[switch_dpid]["routes"].append(route)
+                routing_table[switch_dpid]["suffix_routes"].append(suffix_route)
         switches_aggr.append(pod_switches_aggr)
 
     # create core switches and links
@@ -113,24 +116,25 @@ def main(k):
     for j in range(k//2):
         for i in range(k//2):
             switch_id   = (j*(k//2))+i
-            switch_dpid = f'0000000000{k}{j}{i}'
+            switch_dpid = f'0000000000{k:02x}{j:02x}{i:02x}'
             switch_name = f'crSw{switch_id}'
+            info(f'Creating core switch {switch_name}')
             switch_core = net.addSwitch(switch_name, 
                                         dpid = switch_dpid)
             for num  in range(k):
                 net.addLink(switches_aggr[num][j], switch_core) 
             # add ronte rules for core switches
-            routing_table[switch_name] = { # initial routing table
+            routing_table[switch_dpid] = { # initial routing table
                 "routes": []
             }
             for pod in range(k):
                 route = {
-                    "suffix"     : f'10.{pod}.0.0',
-                    "prefix_len" : 16,
+                    "prefix"     : f'10.{pod}.0.0',
+                    "mask"       : 0xffff0000,
                     "priority"   : priority_down,
                     "output"     : pod + 1
                 }
-                routing_table[switch_name]["routes"].append(route)
+                routing_table[switch_dpid]["routes"].append(route)
 
     with open('routing_table.json', 'w') as f:
         json.dump(routing_table, f, indent=4)
@@ -144,32 +148,16 @@ def main(k):
     net.stop()
 
 def configure_switches(net, k):
-    print('*** Configuring switches')
+    info('*** Configuring switches...')
     num_switches = len(net.switches)
     
-    # edge and aggr switches
-    for j in range(k):
-        for i in range(k // 2):
-            index =  (j * k) + i
-            switch = net.switches[index]
+    with open('swconfig', 'w') as swconfig:
+        for switch in net.switches:
             switch_name = switch.name
-            print(f'*** Configuring {switch_name}')
+            info(f'*** Configuring {switch_name}')
             set_br = f'ovs-vsctl set bridge {switch_name} datapath_type=netdev'
             switch.cmd(set_br)
-        for i in range(k // 2):
-            index =  (j * k) + (k // 2) + i
-            switch = net.switches[index]
-            switch_name = switch.name
-            print(f'*** Configuring {switch_name}')
-            set_br = f'ovs-vsctl set bridge {switch_name} datapath_type=netdev'
-            switch.cmd(set_br)
-
-    # core switches
-    for switch in net.switches[k ** 2 : ]:
-        switch_name = switch.name
-        print(f'*** Configuring {switch_name}')
-        set_br = f'ovs-vsctl set bridge {switch_name} datapath_type=netdev'
-        switch.cmd(set_br)
+            swconfig.write(f'sh {set_br}\n')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
